14c4248af87b4825e0bf38af3fcf1409e52540bc
Liudmyla <112705510+LiashenkoLiudmyla@users.noreply.github.com>
2023-10-02 09:52:20 +0200

    Add files via upload

---
+++ script/bxslider.js
@@ -0,0 +1,23 @@
+const slider = $('.slider-list').bxSlider({
+    mode: 'fade',
+    controls:false,
+    //auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  infiniteLoop: true,
+  //pager: true,
+  touchEnabled: true, 
+});
+
+$(".bx-prev").click ((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+$(".bx-next").click ((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
+
+
+
+
